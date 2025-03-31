f452db4b7da38c8aaf3164860ba341f3a08cf349
tristenfornes <tfornes@email.sc.edu>
2025-03-30 22:18:17 -0400

    Updates

---
+++ precache-manifest.5416c630f7d01bc37d04c596e5cefba5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914545d86e18907ee361bee848e1a4ae",
+    "url": "/react-gameday/index.html"
+  },
+  {
+    "revision": "555d9f77f94d5efc0b3b",
+    "url": "/react-gameday/static/css/main.ae42da67.chunk.css"
+  },
+  {
+    "revision": "8eb0c027ecf851af1f64",
+    "url": "/react-gameday/static/js/2.bf8dc853.chunk.js"
+  },
+  {
+    "revision": "555d9f77f94d5efc0b3b",
+    "url": "/react-gameday/static/js/main.7b21dc5b.chunk.js"
+  },
+  {
+    "revision": "9544d5ef219f5ba1518d",
+    "url": "/react-gameday/static/js/runtime~main.bb436cc5.js"
+  },
+  {
+    "revision": "21a37b8a9cd57ad0115efdfd52b1e64e",
+    "url": "/react-gameday/static/media/comingup.21a37b8a.jpeg"
+  },
+  {
+    "revision": "1e076e2085baa65c42be6f5e8535dff4",
+    "url": "/react-gameday/static/media/fanreaction1.1e076e20.jpeg"
+  },
+  {
+    "revision": "703e299cec1f9610807a11a2c68c5ba9",
+    "url": "/react-gameday/static/media/fanreaction2.703e299c.jpeg"
+  },
+  {
+    "revision": "c8ae1e46aeb5708742ed11b1052a6365",
+    "url": "/react-gameday/static/media/fanreaction3.c8ae1e46.jpeg"
+  },
+  {
+    "revision": "d79eaef20d0e6599cc2c0fca33cff241",
+    "url": "/react-gameday/static/media/fanreaction4.d79eaef2.jpeg"
+  },
+  {
+    "revision": "f844acf0d1a020fd630149386e41cad3",
+    "url": "/react-gameday/static/media/founders.f844acf0.webp"
+  },
+  {
+    "revision": "1d3039ec9e950c0dc0a334219e412de5",
+    "url": "/react-gameday/static/media/goal.1d3039ec.webp"
+  },
+  {
+    "revision": "77257210697170e0c6724adecc842001",
+    "url": "/react-gameday/static/media/homee.77257210.jpeg"
+  },
+  {
+    "revision": "65a202a04b74609ef63248755b9beecf",
+    "url": "/react-gameday/static/media/livenews1.65a202a0.jpeg"
+  },
+  {
+    "revision": "ca5001cb26224fd0ce56e02de8b57572",
+    "url": "/react-gameday/static/media/livenews2.ca5001cb.jpeg"
+  },
+  {
+    "revision": "170cd4227ebc5a159c4eba267be4db31",
+    "url": "/react-gameday/static/media/livenews3.170cd422.jpeg"
+  },
+  {
+    "revision": "a07b7791e0cca8cc37d49959ea78f27d",
+    "url": "/react-gameday/static/media/livenews4.a07b7791.jpeg"
+  },
+  {
+    "revision": "24397418da1366172cec4392d4f59a89",
+    "url": "/react-gameday/static/media/story.24397418.jpeg"
+  }
+]);
